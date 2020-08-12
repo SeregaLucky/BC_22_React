e2da593f561b92d01eed2b9e8d1749de44e5d2a8
SeregaLucky <strizgsergey@gmail.com>
2020-08-12 15:26:23 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+console.log(styles);
+
+class App extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     toogle: true,
+  //   };
+
+  //    change = () => {
+  //      // this.state.toogle = false;
+  //      this.setState({ toogle: !this.state.toogle });
+  //    };
+  // }
+
+  state = {
+    toogle: true,
+  };
+
+  change = () => {
+    // this.state.toogle = false; // ПЛОХО
+    this.setState({ toogle: !this.state.toogle });
+  };
+
+  render() {
+    return (
+      <div className={styles.cont}>
+        <h1 className={styles.title}>Title</h1>
+        <h1>Title222</h1>
+
+        <button onClick={this.change} type="button">
+          Click
+        </button>
+
+        {this.state.toogle && <p>111</p>}
+      </div>
+    );
+  }
 }
 
 export default App;
