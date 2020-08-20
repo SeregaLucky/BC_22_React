a904864dbf772398b83108df14006912936d7da2
SeregaLucky <strizgsergey@gmail.com>
2020-08-20 15:12:33 +0300

    add

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Main</Link>
+          </li>
+
+          <li>
+            <Link to="/contacts">Contacts</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
